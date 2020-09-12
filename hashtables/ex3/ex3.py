2fc67ff1edddd6073cc63fd4a3ae5c7b76fbb174
Andrei Lysenko <andrei.lysenko@outlook.com>
2020-09-12 13:21:59 -0700

    STRETCH: ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -6,12 +6,28 @@ def intersection(arrays):
     storage = {}
     base = arrays[0]
     result = []
-    for x in base:
-        storage[x] = arrays[x]
-
-    print(storage)
+    for arr in arrays:
+        subStorage = {}
+        for i in arr:
+            subStorage[i]=i
+        storage[arrays.index(arr)] = subStorage
+    for i in base:
+        count = 0
+        for x in storage:
+            if i in storage[x]:
+                count +=1
+        if count >= len(storage):
+            result.append(i)
     return result
 
+# if __name__ == "__main__":
+#     arrays = []
+
+#     arrays.append( [1, 2, 3, 5, 8, 10])
+#     arrays.append( [1, 2, 3, 14, 23, 48])
+#     arrays.append( [1, 2, 3, 42, 56, 92])
+
+#     print(intersection(arrays))
 
 if __name__ == "__main__":
     arrays = []
